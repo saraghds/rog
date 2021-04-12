c0204df9a0e2d2303d7ab3a0816f60d039f3dac8
saraghds <sara.ghodsi@gmail.com>
2021-04-12 19:01:59 -0400

    fix bug

--- src/main/java/com/buzzfuzz/rog/traversal/InstanceDispatcher.java
+++ src/main/java/com/buzzfuzz/rog/traversal/InstanceDispatcher.java
@@ -19,47 +19,47 @@ import com.buzzfuzz.rog.decisions.Constraint;
 import com.buzzfuzz.rog.decisions.Target;
 
 public class InstanceDispatcher {
-	
-    private Set<ClassPkg> history;
-    private Target context;
-    private Constraint constraint;
-    private ROG robjg;
-    private RNG rng;
 
-    public InstanceDispatcher(ROG rog) {
-        this(new RNG(), rog);
-    }
-	
+	private Set<ClassPkg> history;
+	private Target context;
+	private Constraint constraint;
+	private ROG robjg;
+	private RNG rng;
+
+	public InstanceDispatcher(ROG rog) {
+		this(new RNG(), rog);
+	}
+
 	public InstanceDispatcher(RNG rng, Set<ClassPkg> chain, ROG robjg) {
 		this.rng = rng;
-        this.history = chain == null ? new LinkedHashSet<ClassPkg>() : new LinkedHashSet<ClassPkg>(chain);
-        this.robjg = robjg;
-    }
+		this.history = chain == null ? new LinkedHashSet<ClassPkg>() : new LinkedHashSet<ClassPkg>(chain);
+		this.robjg = robjg;
+	}
 
 	public InstanceDispatcher(RNG rng, ROG robjg) {
 		this(rng, null, robjg);
 	}
-	
+
 	public InstanceDispatcher(InstanceDispatcher dispatcher) {
 		this(dispatcher.rng, dispatcher.history, dispatcher.robjg);
 	}
-	
+
 	public InstanceDispatcher(InstanceFinder finder) {
 		this(finder.rng, finder.history, finder.rog);
 	}
-	
+
 	public Set<ClassPkg> getHistory() {
 		return history;
 	}
-	
+
 	public RNG getRNG() {
 		return rng;
-    }
+	}
 
-    public ROG getROG() {
-        return robjg;
-    }
-	
+	public ROG getROG() {
+		return robjg;
+	}
+
 	private void log(String msg) {
 		int indent = history.size();
 		while (indent > 0) {
@@ -70,52 +70,55 @@ public class InstanceDispatcher {
 	}
 
 	public Object tryGetInstance(Type type) {
-        ClassPkg target = parseType(type);
+		ClassPkg target = parseType(type);
 
 		if (history.contains(target))
-            return null;
+			return null;
 
 		// Maybe in load method?
 		history.add(target);
 		loadConstraint(getContext(target.getClazz()));
 
 		if (!target.getClazz().isPrimitive() && constraint.isNull() != null && constraint.isNull()) {
-            log("Returning null instead of instance");
-            Choice nullChoice = new Choice();
-            nullChoice.setTarget(this.context);
-            nullChoice.setValue();
-            rng.getConfig().getChoices().add(nullChoice);
-            return null;
+			log("Returning null instead of instance");
+			Choice nullChoice = new Choice();
+			nullChoice.setTarget(this.context);
+			nullChoice.setValue();
+			rng.getConfig().getChoices().add(nullChoice);
+			return null;
 		}
-		
-		return getInstance(target);
-    }
 
-    public Object getInstance(Type type) {
-        // This is probably an indication that I can't get the same kind of generics information from just getClass().
-        // Which makes sense.
-        // The end solution is probably to reserve this method to only work for non-generic types and then
-        // Research ways to reference a field or some kind of lightweight object
-        // Could even automatically make a field a type within my own custom class to make it easier...
-        return getInstance(parseType(type));
-    }
+		return getInstance(target);
+	}
+
+	public Object getInstance(Type type) {
+		// This is probably an indication that I can't get the same kind of generics
+		// information from just getClass().
+		// Which makes sense.
+		// The end solution is probably to reserve this method to only work for
+		// non-generic types and then
+		// Research ways to reference a field or some kind of lightweight object
+		// Could even automatically make a field a type within my own custom class to
+		// make it easier...
+		return getInstance(parseType(type));
+	}
 
 	private Object getInstance(ClassPkg target) {
-		
+
 		// Might need to move history check here
-		
+
 		// If this method was called directly
-		if(constraint == null) {
+		if (constraint == null) {
 			history.add(target);
 			loadConstraint(getContext(target.getClazz()));
 		}
-		
+
 		Object instance = checkPrimatives(target.getClazz());
-		
+
 		if (instance == null) {
 			instance = checkCommon(target);
 		}
-		
+
 		if (instance == null) {
 			// Eventually will need full ClassPkg
 			instance = checkClasses(target.getClazz());
@@ -131,35 +134,35 @@ public class InstanceDispatcher {
 			if (instance.getGenerics() != null && instance.getGenerics().length != 0) {
 				instancePath += '<';
 				for (Type generic : instance.getGenerics()) {
-					instancePath += generic.getTypeName().substring(generic.getTypeName().lastIndexOf('.')+1) + ',';
+					instancePath += generic.getTypeName().substring(generic.getTypeName().lastIndexOf('.') + 1) + ',';
 				}
-				instancePath = instancePath.substring(0, instancePath.length()-1);
+				instancePath = instancePath.substring(0, instancePath.length() - 1);
 				instancePath += '>';
 			}
 			instancePath += '.';
 		}
-		instancePath = instancePath.substring(0, instancePath.length()-1);
+		instancePath = instancePath.substring(0, instancePath.length() - 1);
 		context.setInstancePath(instancePath);
-        log("current path: " + instancePath);
+		log("current path: " + instancePath);
 
-        context.setTypeName(target.getSimpleName());
-		
+		context.setTypeName(target.getSimpleName());
+
 		return context;
 	}
-	
+
 	private void loadConstraint(Target target) {
 		Constraint constraint = rng.getConstraint(target);
-		
+
 		if (constraint == null) {
 //			System.out.println("MAKING NEW CONSTRAINT");
 			constraint = rng.makeConstraint(target);
-		} 
+		}
 //		else System.out.println("USING EXISTING CONSTRAINT" + constraint.toString());
 		this.constraint = constraint;
 	}
-	
+
 	public Object checkClasses(Class<?> target) {
-		
+
 		Object inst = new FuzzConstructorFinder(this).findInstance(target);
 		if (inst == null) {
 			inst = new ConstructorFinder(this).findInstance(target);
@@ -169,32 +172,35 @@ public class InstanceDispatcher {
 					inst = new FactoryFinder(this).findInstance(target);
 					if (inst == null) {
 						inst = new SubclassFinder(this).findInstance(target);
-						if (inst == null)
+						if (inst == null) {
 							inst = createInstanceFromFields(target);
 							if (inst == null)
 								log("Could not find a way to get an instance of this class.");
+						}
 					}
 				}
 			}
 		}
 		return inst;
 	}
-	
+
 	public Object createInstanceFromFields(Class<?> target) {
 		Object obj = null;
-		
-		try {
-			Field[] fields = target.getClass().getDeclaredFields();
-			for (Field field : fields) {
-				field.setAccessible(true);
+
+		Field[] fields = target.getClass().getDeclaredFields();
+		for (Field field : fields) {
+			field.setAccessible(true);
+			try {
 				Object fieldObj = getInstance(field.getGenericType());
 				field.set(obj, fieldObj);
+			} catch (Exception e) {
+				continue;
 			}
-		} catch (Exception e) { }
-	
+		}
+
 		return obj;
 	}
-	
+
 	public Object checkPrimatives(Class<?> target) {
 		if (target.equals(int.class)) {
 			return rng.getInt(this.context, this.constraint);
@@ -215,25 +221,25 @@ public class InstanceDispatcher {
 		} else if (target.equals(String.class)) {
 			return rng.getString(this.context, this.constraint);
 		} else if (target.isEnum()) {
-            return rng.getEnum(this.context, this.constraint, target);
+			return rng.getEnum(this.context, this.constraint, target);
 		} else {
 			return null;
 		}
 	}
 
 	private Object checkCommon(ClassPkg target) {
-		
+
 		if (target.getClazz().isArray()) {
 			Class<?> type = target.getClazz().getComponentType();
 			return randomArray(type);
 		} else if (target.getClazz().equals(List.class) || target.getClazz().equals(ArrayList.class)) {
 //			log(String.valueOf(target.getGenerics() == null));
 			ClassPkg type = parseType(target.getGenerics()[0]);
-			
+
 			// Unfortunately this needs to be separate from the randArray method because
 			// there can be arrays of primitives but not Lists of primitives
 			int length = rng.fromRange(0, 10);
-			Object[] array = (Object[])Array.newInstance(type.getClazz(), length);
+			Object[] array = (Object[]) Array.newInstance(type.getClazz(), length);
 			for (int i = 0; i < length; i++) {
 				Object instance = new InstanceDispatcher(this).getInstance(type);
 				if (instance == null) {
@@ -249,7 +255,7 @@ public class InstanceDispatcher {
 		}
 		return null;
 	}
-	
+
 	private Object randomArray(ClassPkg type) {
 		int length = rng.fromRange(0, 10);
 		Object array = Array.newInstance(type.getClazz(), length);
@@ -266,47 +272,49 @@ public class InstanceDispatcher {
 
 	private Object randomArray(Class<?> type) {
 		return randomArray(new ClassPkg(type, null));
-    }
+	}
 
-    public static ClassPkg parseType(Type type) {
-        ClassPkg pkg;
-        if (type instanceof Class) {
-            // This doesn't have generics.
-            pkg = new ClassPkg((Class<?>)type, null);
-        } else if (type instanceof ParameterizedType){
-            ParameterizedType pt = (ParameterizedType)type;
-            // Eventually will have to store ClassPkgs here to account for List<List<Integer>>
-            Type[] generics = new Type[pt.getActualTypeArguments().length];
-            for (int j=0; j < pt.getActualTypeArguments().length; j++) {
-                Type gtype = pt.getActualTypeArguments()[j];
-                if (gtype instanceof WildcardType) {
-                    // Generic is in "? extends Class" format. We want its upperbound
-                    WildcardType wc = (WildcardType)gtype;
-                    // if (wc.getUpperBounds()[0] == null)
-                    // 	log("New kind of wildcard");
-                    generics[j] = wc.getUpperBounds()[0];
-                } else {
-                    // Generic is a normal class at this point (probably)
-                    generics[j] = gtype;
-                }
-            }
-            pkg = new ClassPkg((Class<?>)pt.getRawType(), generics);
-        } else {
-            // TODO: This is a generic type E. Will need to feed in a random object
-            pkg = new ClassPkg(String.class, null);
-        }
+	public static ClassPkg parseType(Type type) {
+		ClassPkg pkg;
+		if (type instanceof Class) {
+			// This doesn't have generics.
+			pkg = new ClassPkg((Class<?>) type, null);
+		} else if (type instanceof ParameterizedType) {
+			ParameterizedType pt = (ParameterizedType) type;
+			// Eventually will have to store ClassPkgs here to account for
+			// List<List<Integer>>
+			Type[] generics = new Type[pt.getActualTypeArguments().length];
+			for (int j = 0; j < pt.getActualTypeArguments().length; j++) {
+				Type gtype = pt.getActualTypeArguments()[j];
+				if (gtype instanceof WildcardType) {
+					// Generic is in "? extends Class" format. We want its upperbound
+					WildcardType wc = (WildcardType) gtype;
+					// if (wc.getUpperBounds()[0] == null)
+					// log("New kind of wildcard");
+					generics[j] = wc.getUpperBounds()[0];
+				} else {
+					// Generic is a normal class at this point (probably)
+					generics[j] = gtype;
+				}
+			}
+			pkg = new ClassPkg((Class<?>) pt.getRawType(), generics);
+		} else {
+			// TODO: This is a generic type E. Will need to feed in a random object
+			pkg = new ClassPkg(String.class, null);
+		}
 
-        return pkg;
-    }
-	
+		return pkg;
+	}
+
 	public static ClassPkg[] packageClasses(Type[] genArgs) {
-		// TODO: Sometimes target or generics is null. Maybe this is because of cases like 'E'
+		// TODO: Sometimes target or generics is null. Maybe this is because of cases
+		// like 'E'
 
 		// We are creating one classPkg per argument
 		ClassPkg[] pkgs = new ClassPkg[genArgs.length];
-		
+
 		// Proposing new way to do generics based on what I know now
-		for (int i=0; i < genArgs.length; i++) {
+		for (int i = 0; i < genArgs.length; i++) {
 			Type type = genArgs[i];
 			pkgs[i] = parseType(type);
 		}
@@ -314,7 +322,7 @@ public class InstanceDispatcher {
 		return pkgs;
 	}
 
-    // I can probably make methods like these private. It would be best...
+	// I can probably make methods like these private. It would be best...
 	public Object[] randomArgs(Type[] genArgs) {
 
 		Object[] instances = new Object[genArgs.length];
